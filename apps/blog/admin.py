e623a0bc75cde42d51e7a6605089d81f52067798
yogi sofi harja <hardja.ogie@yahoo.com>
2012-02-10 00:55:08 +0700

    nambah model post, event

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from blog.models import Kategori
+from blog.models import Kategori, Post, Event
 
 admin.site.register(Kategori) 
+admin.site.register(Post)
+admin.site.register(Event)
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.admin.models import User
 class Kategori(models.Model):
     nama = models.CharField(max_length=255)
     
@@ -8,3 +8,22 @@ class Kategori(models.Model):
 
     class Meta :
 	verbose_name_plural = "Daftar Kategori"
+
+class Post(models.Model):
+    pub_date = models.DateField(auto_now_add=True)
+    judul = models.CharField(max_length=200)
+    konten = models.TextField()
+    status = models.BooleanField()
+    user = models.ForeignKey(User)
+    kategori = models.ForeignKey("Kategori")
+    
+    def __unicode__(self):
+	return self.judul
+
+class Event(models.Model):
+    nama = models.CharField(max_length=200)
+    tanggal = models.DateField()
+    keterangan = models.TextField()
+    
+    def __unicode__(self):
+	return self.nama
