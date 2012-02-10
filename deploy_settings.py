39fbe1ac01604bb0e4eb70627a54880a7418008f
Bramandityo Prabowo <freez_meinster@yahoo.co.id>
2012-02-10 02:57:30 +0000

    modifikasi deploy_settings.py

--- deploy_settings.py
+++ deploy_settings.py
@@ -2,20 +2,20 @@
 
 import os
 import sys
+import settings
 
 PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
 sys.path.insert(0,os.path.join(PROJECT_ROOT,'apps'))
 sys.path.insert(0,os.path.join(PROJECT_ROOT,'libs'))
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+DEBUG = settings.DEBUG
+TEMPLATE_DEBUG = settings.TEMPLATE_DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
-MANAGERS = ADMINS
-
+MANAGERS = 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
@@ -27,122 +27,56 @@ DATABASES = {
     }
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'Asia/Jakarta'
+TIME_ZONE = settings.TIME_ZONE
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'id'
+LANGUAGE_CODE = settings.LANGUAGE_CODE
 
-SITE_ID = 1
+SITE_ID = settings.SITE_ID
 
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
+USE_I18N = settings.USE_I18N
 
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = True
+USE_L10N = settings.USE_L10N
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = settings.MEDIA_ROOT
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = settings.MEDIA_URL
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'asset')
+STATIC_ROOT = settings.STATIC_ROOT
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = settings.STATIC_URL
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = settings.ADMIN_MEDIA_PREFIX
 
 # Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+STATICFILES_DIRS = settings.STATICFILES_DIRS
 
 # List of finder classes that know how to find static files in
 # various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
+STATICFILES_FINDERS = settings.STATICFILES_FINDERS
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '7k2on438)_s%9z#-f0a)^&4n5=#+t=*!6@ik8t2l(=+)uxqwiu'
+SECRET_KEY = settings.SECRET_KEY
 
 # List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
+TEMPLATE_LOADERS = settings.TEMPLATE_LOADERS
 
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-)
+MIDDLEWARE_CLASSES = settings.MIDDLEWARE_CLASSES
 
-ROOT_URLCONF = 'avagata.urls'
+ROOT_URLCONF = settings.ROOT_URLCONF
 
-TEMPLATE_DIRS = (
-    os.path.join(PROJECT_ROOT, 'templates')
-)
+TEMPLATE_DIRS = settings.TEMPLATE_DIRS
 
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-    'blog'
-)
-
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
+INSTALLED_APPS = settings.INSTALLED_APPS
+LOGGING = settings.LOGGING
--- settings.py
+++ settings.py
@@ -121,7 +121,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'blog'
+    'blog',
+    'landing'
 )
 
 # A sample logging configuration. The only tangible logging
